eebe6b7118c5c9e22efcadcf15b6cc267615f53a
ethanCART <ethan.brass@student.cart.org>
2022-03-14 09:00:27 -0700

    e

--- main.js
+++ main.js
@@ -8,4 +8,38 @@ function myFunction() {
 	}
   }
 
-  /*localisation*/
+    // new JS, trying to make a play butotn
+  var x = document.getElementById("beeg");//start button 
+  var restart = document.getElementById("restart");//restart button
+  var playArea = document.getElementById("playArea");//refers to play area
+  var buttonArea = document.getElementById("buttonArea");//refers to button area
+  
+  function createButton(btnText, choice) {
+	var button = document.createElement("button"); //creates button
+	button.innerHTML = btnText;//changes button text according to "options" in array
+	buttonArea.appendChild(button);//adds button that has changed name
+  
+	button.addEventListener("click", function(){//when button that is generated is clicked, add choice to playerChoices and run createStory
+	  playerChoices.push(choice);
+	  createStory();
+	  window.scrollTo({ top: 0, behavior: 'smooth' });//shamelessly stolen from stackOverflow, scrolls to top smoothly on button press
+	});
+  }
+  
+  function addStory(text){//changes playArea text to choice
+	playArea.innerHTML = text;
+  }
+  
+  function createStory(text){
+	let pageNow = playerChoices[playerChoices.length - 1];
+	playArea.innerHTML = "";//empties play area
+	buttonArea.innerHTML = "";//empties button area
+	for(let idea of playerChoices){//creates variable idea and uses with array playerChoices
+	  addStory(story[idea].text)//based on button clicked, run addStory with text
+	}
+  
+  }
+
+  x.addEventListener("click", function(){//on start button click
+	createStory(story.opener.text);//run createStory
+  });
